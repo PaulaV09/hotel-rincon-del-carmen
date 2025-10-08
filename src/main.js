897713320310bb447f74dde97d5abc2a0048f7b1
PaulaV09 <98952162+PaulaV09@users.noreply.github.com>
2025-10-07 23:59:24 -0500

    feat: :sparkles: Agregar ruta de redireccionamiento del componente de detalle en main.js

--- src/main.js
+++ src/main.js
@@ -3,3 +3,5 @@ import "../js/components/footer-component.js";
 import "../js/components/contacto-component.js";
 import "../js/components/landing-component.js";
 import "../js/components/booking-component.js";
+import "../js/components/detalle-component.js";
+
