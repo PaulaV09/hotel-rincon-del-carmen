d091712034d18a7efc45c55b9be9e1dcc57b481e
PaulaV09 <98952162+PaulaV09@users.noreply.github.com>
2025-10-07 22:29:31 -0500

    feat: :sparkles: Agregar ruta de direccionamiento al componente del landing page en el main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import "../js/components/nav-component.js";
 import "../js/components/footer-component.js";
-import "../js/components/contacto-component.js"
+import "../js/components/contacto-component.js";
+import "../js/components/landing-component.js";
