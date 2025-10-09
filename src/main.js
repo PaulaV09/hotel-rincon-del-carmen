3d5dad8aca76c691f984a1095ccbeb9c3637a168
PaulaV09 <98952162+PaulaV09@users.noreply.github.com>
2025-10-08 20:49:41 -0500

    feat: :sparkles: Agregar rutas de redireccionamiento de varios componentes en main.js

--- src/main.js
+++ src/main.js
@@ -4,4 +4,6 @@ import "../js/components/contacto-component.js";
 import "../js/components/landing-component.js";
 import "../js/components/booking-component.js";
 import "../js/components/detalle-component.js";
+import "../js/components/mis-reservas-component.js";
+import "../js/components/perfil-component.js";
 
